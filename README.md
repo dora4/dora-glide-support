0961cda3821ebe85e2e5498c13ad5160bb8cbe31
dora <dora924666990@gmail.com>
2025-08-16 19:08:17 +0800

    升级dora主框架版本

--- README.md
+++ README.md
@@ -17,7 +17,7 @@ dependencyResolutionManagement {
 dependencies {
     // 扩展包必须在有主框架dora的情况下使用
     implementation("com.github.dora4:dora:1.3.14")
-    implementation("com.github.dora4:dora-glide-support:1.4")
+    implementation("com.github.dora4:dora-glide-support:1.5")
 }
 ```
 
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "dora.lifecycle.glide"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         minSdk = 21
@@ -25,7 +25,7 @@ android {
 }
 
 dependencies {
-    implementation("com.github.dora4:dora:1.1.46")
+    implementation("com.github.dora4:dora:1.3.14")
     api("com.github.bumptech.glide:glide:4.15.0")
 }
 
@@ -36,7 +36,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.dora4"
                 artifactId = "dora-glide-support"
-                version = "1.4"
+                version = "1.5"
             }
         }
     }
